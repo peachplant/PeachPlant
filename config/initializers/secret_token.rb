8f38fefc06137ae9059e1eaa0bcc6eb3380d5e6d
Francis Byrne <byrnef87@gmail.com>
2012-02-20 15:29:20 +1100

    Fresh RoR install take two

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PeachPlant::Application.config.secret_token = 'a2aa486f09b42408f29c40c40e0622e07506f9225bbcab33f3aac20a19c181f2aa2e43e05955129600605bfd9a326ebdb57e69b28f767c3ee8191a5d6163f8bb'
