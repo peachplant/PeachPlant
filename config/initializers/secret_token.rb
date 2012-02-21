f84ef040d69f57fa370a271cd3f578e4285fe589
John Jiang <johnjiang101@gmail.com>
2012-02-21 20:30:17 +1100

    Updated peach plant to use rails 3.2.1

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-PeachPlant::Application.config.secret_token = 'a2aa486f09b42408f29c40c40e0622e07506f9225bbcab33f3aac20a19c181f2aa2e43e05955129600605bfd9a326ebdb57e69b28f767c3ee8191a5d6163f8bb'
+PeachPlant::Application.config.secret_token = '320963cc1c09295f9658bd5f6a556c8084f4c7bce501435d85888f03c07793df8cb679231b240f602957854353cdabef9545ac8f6577a5ddc307a3afc33fc472'
