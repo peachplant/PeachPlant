d38b31e9dd264ea0f85af0081882ac4ea629fc94
spazkus <nefario>
2012-02-25 17:52:31 +1100

    Adding recaptcha code

---
+++ config/initializers/recaptcha.rb
@@ -0,0 +1,6 @@
+#./config/initializes/recaptcha.rb
+# from github.com/ambethia/recaptcha README.rdoc
+Recaptcha.configure do |config|
+	config.public_key  = '6Lc5IM4SAAAAAAGHBcKeChDx-WN1sWlMzOpLOCK0'
+	config.private_key = '6Lc5IM4SAAAAAIP1Qft9qMB4URCO5PhZbHYDskA1'
+end
