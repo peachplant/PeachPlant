8a2c4aadbaee4c22eada55cf525d1590aaf8a5cd
francisbyrne <byrnef87@gmail.com>
2012-02-28 12:44:40 +1100

    fixed title css removed css negative error in legend add TODO in register form moved TODO in listings helper

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,7 +1,19 @@
 module ListingsHelper
 
+  #TODO: replace image_not_found with stock photo
   #filename of image to display if no image is found
   IMAGE_NOT_FOUND = "image_not_found.jpg"
+
+  #returns the image tag for an listing image id
+  def show_image(image_id)
+    if image_id.nil?
+      image_tag IMAGE_NOT_FOUND
+    else
+      @image_path = get_file_path image_id
+      image_tag @image_path
+    end
+  end
+end
 
   #returns the file path for a listing image_id
   def get_file_path(image_id)
@@ -12,15 +24,3 @@ module ListingsHelper
       image_path "listings/#{@filename}"
     end
   end
-
-  #returns the image tag for an listing image id
-  #TODO: replace image_not_found with stock photo
-  def show_image(image_id)
-    if image_id.nil?
-      image_tag IMAGE_NOT_FOUND
-    else
-      @image_path = get_file_path image_id
-      image_tag @image_path
-    end
-  end
-end
